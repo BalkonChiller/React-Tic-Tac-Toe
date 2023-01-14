97f7f7021c2945ff9cde4463fe71429d48d447f7
BalkonChiller <46871785+BalkonChiller@users.noreply.github.com>
2023-01-14 21:32:32 +0100

    Feature fail 2

--- src/index.js
+++ src/index.js
@@ -61,12 +61,12 @@ class Game extends React.Component {
     this.state = {
       history: [{ squares: Array(9).fill(null) }],
       xIsNext: true,
-      stepCount: 0,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepCount + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -79,21 +79,21 @@ class Game extends React.Component {
           squares: squares,
         },
       ]),
-      stepCount: history.length,
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
   jumpTo(step) {
     this.setState = {
-      stepCount: step,
+      stepNumber: step,
       xIsNextx: (step % 2) === 0,
     };
   }
 
   render() {
     const history = this.state.history;
-    const current = history[this.state.stepCount];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
